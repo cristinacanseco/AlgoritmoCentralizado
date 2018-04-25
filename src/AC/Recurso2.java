2cb6f38495e507d574220ab3cfcf77a02658a235
Cristy <Cristy@192.168.1.70>
2018-04-25 07:47:28 -0500

    -La versión chola

--- src/AC/Recurso2.java
+++ src/AC/Recurso2.java
@@ -29,12 +29,39 @@ public class Recurso2 extends Thread {
     
     public void run(){
         //Revisar el recurso
+        String lista="";
         while(true){
-            try {
+            
                 if(!cola.isEmpty()){
                     if(recurso == -1){                      
-                        recurso = cola.get(0);
-                        liberarRecurso();         
+                        try {
+                            recurso = cola.get(0);
+                            GUI.jTextFieldRecurso.setText(""+recurso);
+                            if(!cola.isEmpty()){
+                                lista="";
+                                System.out.println("La cola de peticiones actualizada es: ");
+                                for(int i=1; i<cola.size(); i++){
+                                    System.out.println("CA "+cola.get(i));
+                                    lista += "  " + cola.get(i);
+                                }
+                                GUI.jTextFieldLista.setText(lista);
+
+                            }else{
+                                GUI.jTextFieldRecurso.setText(""+recurso);
+                                System.out.println("La cola de peticiones actualizada está vacía ");
+                                lista = "Cola de peticiones vacía";
+                                GUI.jTextFieldLista.setText(lista);
+                            }
+                            Random ran = new Random();
+                            int t = ran.nextInt(5000)+3000;
+                            Thread.sleep(t);
+                            liberarRecurso();
+                        } catch (InterruptedException ex) {
+                            Logger.getLogger(Recurso2.class.getName()).log(Level.SEVERE, null, ex);
+                        } catch (IndexOutOfBoundsException ex2){
+                            System.out.println(ex2.getMessage());
+                        }
+                       
                     }else{
                         try {
                             //Agrega a la cola
@@ -44,21 +71,22 @@ public class Recurso2 extends Thread {
                         }
                     }
                 }
-                wait();
-            } catch (InterruptedException ex) {
-                Logger.getLogger(Recurso2.class.getName()).log(Level.SEVERE, null, ex);
-            }
+                //wait();
+            
         }
     }
     public void liberarRecurso(){
-        String lista="";
         try {
-            System.out.println("El recurso está siendo utilizado por : " +recurso);
-            GUI.jTextFieldRecurso.setText(""+recurso);
+            String lista="";
+            
+            //System.out.println("El recurso está siendo utilizado por : " +recurso);
+            //GUI.jTextFieldRecurso.setText(""+recurso);
+            
             Random ran = new Random();
-            int t = ran.nextInt(10000);
+            int t = ran.nextInt(5000)+2000;
             Thread.sleep(t);
             recurso = -1;
+            GUI.jTextFieldRecurso.setText(""+recurso);
             cola.remove(0);
 
             if(!cola.isEmpty()){
@@ -70,13 +98,15 @@ public class Recurso2 extends Thread {
                 GUI.jTextFieldLista.setText(lista);
                 
             }else{
+                GUI.jTextFieldRecurso.setText(""+recurso);
                 System.out.println("La cola de peticiones actualizada está vacía ");
                 lista = "Cola de peticiones vacía";
                 GUI.jTextFieldLista.setText(lista);
             }
-            
         } catch (InterruptedException ex) {
-            System.out.println(ex.getMessage());
+            Logger.getLogger(Recurso2.class.getName()).log(Level.SEVERE, null, ex);
         }
+            
+        
     }
 }
